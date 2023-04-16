a65f857f26d3e7ad9040459b46b305bc6b674a35
Uma Dhatri <97341880+umadhatri@users.noreply.github.com>
2023-04-16 09:03:51 +0000

    backend

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT||5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.use('/openai', require('./routes/openAIroutes'));
 
 app.listen(port, ()=>console.log(`Server started on port ${port}`));
+
